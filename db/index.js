fa41f00eb983eb9201dd7ecbce519ce5df72b93d
BenPuusta <benpuusta@gmail.com>
2022-11-22 12:37:27 +0000

    Got backend communicating with SQL DB

---
+++ db/index.js
@@ -0,0 +1,9 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_CONNECTION_URL,
+});
+
+export function query(text, params) {
+  return pool.query(text, params);
+}
--- routes/questionRouter.js
+++ routes/questionRouter.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 const router = Router();
+import { getQs } from "../models/questionMods.js"
 
 router.get ("/questions", async function (req, res){
 console.log ("Got to QRouter!")
-
+const result = await getQs()
+res.json({success: true, payload: result})
 })
 
 
